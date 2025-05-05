bffaeea57cdded1de1557bc01471f5dff7867acb
NaraAndrad3 <naradiasspn1967@gmail.com>
2025-05-05 13:39:20 -0300

    Correção e atualização do consumidor de imagens para reconhecimento de emoções

--- consumer_face/facer.py
+++ consumer_face/facer.py
@@ -4,18 +4,22 @@ import pika
 import time
 from deepface import DeepFace
 import os
+import uuid
 
 # Configurações do RabbitMQ
 rabbitmq_host = 'rabbitmq'
 exchange_name = 'mensagens'
+
 routing_key = 'face'
 queue_name = 'fila_sentimentos'
-image_path = '/app/images/faces' # Caminho dentro do container
+image_path = '/app/processed_images/faces'
 
-def analisar_sentimento(img_name):
+
+os.makedirs(image_path, exist_ok=True)
+
+def analisar_sentimento(img_path):
     try:
-        img_path = os.path.join(image_path, img_name)
-        result = DeepFace.analyze(img_path=img_path, actions=['emotion'], silent=True)
+        result = DeepFace.analyze(img_path=img_path, actions=['emotion'], silent=True, enforce_detection=False)
         if result:
             dominant = result[0]["dominant_emotion"]
             return f"Emoção dominante: {dominant}"
@@ -25,11 +29,19 @@ def analisar_sentimento(img_name):
         return f"Erro ao analisar a emoção: {e}"
 
 def callback(ch, method, properties, body):
-    mensagem = body.decode()
-    print(f"Mensagem recebida (Sentimentos): {mensagem}")
-    resultado_ia = analisar_sentimento(mensagem)
-    print(f"Análise de Sentimento da mensagem: {mensagem}: {resultado_ia}")
-    time.sleep(1) 
+    
+    image_name = f"received_face_{uuid.uuid4().hex}.jpg"
+    file_path = os.path.join(image_path, image_name)
+
+    with open(file_path, 'wb') as f:
+        f.write(body)
+    print(f"Imagem de rosto recebida e salva como: {file_path}")
+
+    
+    resultado_ia = analisar_sentimento(file_path)
+    print(f"Análise de Sentimento da imagem {image_name}: {resultado_ia}")
+    time.sleep(1)
+
 def connect_rabbitmq():
     while True:
         try:
@@ -47,7 +59,7 @@ connection, channel = connect_rabbitmq()
 
 try:
     channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=False)
 
     print(' [*] Aguardando mensagens (Sentimentos). Para sair, pressione CTRL+C')
     channel.start_consuming()
